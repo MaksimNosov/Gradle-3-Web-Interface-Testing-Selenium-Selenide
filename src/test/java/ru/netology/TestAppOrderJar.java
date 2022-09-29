76f8a82d0bfbc2f9d427de5e644c03fe32bedb83
MaksimNosov <m.s.nosov@gmail.com>
2022-09-29 20:43:06 +0300

    Change appveyor.yml for selenid, Change build by headles for selenid, change tests

--- src/test/java/ru/netology/TestAppOrderJar.java
+++ src/test/java/ru/netology/TestAppOrderJar.java
@@ -1,5 +1,7 @@
 package ru.netology;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -13,62 +15,83 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.List;
 
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestAppOrderJar {
-//    WebDriver driver;
-//
-//    @BeforeAll
-//    static void setupAll() {
-//        WebDriverManager.chromedriver().setup();
+
+    //    @Test
+//    void testNameNotTrue() {                     /// тест на Selenium проходит
+//        driver.get("http://localhost:9999/");
+//        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("English");
+//        driver.findElement(By.className("button__text")).click();
+//        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+//        String actual = driver.findElement(By.className("input__sub")).getText().trim();
+//        assertEquals(expected, actual);
 //    }
-//
-//    @BeforeEach
-//    void setup() {
-//        driver = new ChromeDriver();
+    @Test
+    void testNameNotTrue() {
+        open("http://localhost:9999/");
+        SelenideElement form = $("[id=root]");
+        form.$("[id=root] input").setValue("English");
+        form.$("[id=root] button").click();
+        $(".input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+//    @Test
+//    void testNoName() {                     /// тест на Selenium проходит
+//        driver.get("http://localhost:9999/");
+//        driver.findElement(By.className("button__text")).click();
+//        String expected = "Поле обязательно для заполнения";
+//        String actual = driver.findElement(By.className("input__sub")).getText().trim();
+//        assertEquals(expected, actual);
 //    }
-//
-//    @AfterEach
-//    void teardown() {
-//        driver.quit();
-//    }
-
-    WebDriver driver;
-
-    @BeforeAll
-    static void setupAll() {
-        WebDriverManager.chromedriver().setup();
-    }
-
-    @BeforeEach
-    void setup() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
-//        driver = new ChromeDriver();
-    }
-
-    @AfterEach
-    void teardown() {
-        driver.quit();
-    }
 
     @Test
-    void test() {
-        // Your test logic here
+    void testNoName() {
+        open("http://localhost:9999/");
+        SelenideElement form = $("[id=root]");
+        form.$("[id=root] button").click();
+        $(".input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
+//    @Test
+//    void testPhoneNotTrue() {                     /// тест на Selenium проходит
+//        driver.get("http://localhost:9999/");
+//        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Точно как в паспорте");
+//        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("9999999999999");
+//        driver.findElement(By.className("button__text")).click();
+//        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+//        String actual = driver.findElement(By.cssSelector("[data-test-id=phone] .input__sub")).getText().trim();
+//        assertEquals(expected, actual);
+//    }
+
     @Test
-    void shouldTestV1() {
-        driver.get("http://localhost:9999/");
-        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Точно как в паспорте");
-        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79998889988");
-        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
-        driver.findElement(By.className("button__text")).click();
-        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
-        assertEquals(expected, actual);
+    void testPhoneNotTrue() {
+        open("http://localhost:9999/");
+        SelenideElement form = $("[id=root]");
+        form.$("[data-test-id=name] input").setValue("Точно как в паспорте");
+        form.$("[data-test-id=phone] input").setValue("9999999999999");
+        form.$("[id=root] button").click();
+        $("[id=root] .input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+//
+//    @Test
+//    void testNoPhone() {                     /// тест на Selenium проходит
+//        driver.get("http://localhost:9999/");
+//        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Точно как в паспорте");
+//        driver.findElement(By.className("button__text")).click();
+//        String expected = "Поле обязательно для заполнения";
+//        String actual = driver.findElement(By.cssSelector("[data-test-id=phone] .input__sub")).getText().trim();
+//        assertEquals(expected, actual);
+//    }
+        @Test
+    void testNoPhone() {
+            open("http://localhost:9999/");
+            SelenideElement form = $("[id=root]");
+            form.$("[data-test-id=name] input").setValue("Точно как в паспорте");
+            form.$("[id=root] button").click();
+            $("[id=root] .input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
 }
